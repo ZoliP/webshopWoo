d3a66de2091441b681a9905cbc465b45cda26421
Pallo Zoltan <pallozoli@yahoo.com>
2021-06-14 16:30:07 +0300

    wip search bar, theme buttons, colors

--- wp-content/themes/wooTheme/functions.php
+++ wp-content/themes/wooTheme/functions.php
@@ -30,4 +30,28 @@ function zoltan_woocommerce_theme_support(){
 }
 
 add_action( 'after_setup_theme', 'zoltan_woocommerce_theme_support' );
+
+if ( ! function_exists( 'storefront_cart_link' ) ) {
+	/**
+	 * Cart Link
+	 * Displayed a link to the cart including the number of items present and the cart total
+	 *
+	 * @return void
+	 * @since  1.0.0
+	 */
+	function storefront_cart_link() {
+		if ( ! storefront_woo_cart_available() ) {
+			return;
+		}
+		?>
+			<a class="cart-contents" href="<?php echo esc_url( wc_get_cart_url() ); ?>" title="<?php esc_attr_e( 'View your shopping cart', 'storefront' ); ?>">
+				<?php /* translators: %d: number of items in cart */ ?>
+				<!-- <?php echo wp_kses_post( WC()->cart->get_cart_subtotal() ); ?>  -->
+				<span class="count">
+					<?php echo wp_kses_data( sprintf( _n( '%d item', '%d items', WC()->cart->get_cart_contents_count(), 'storefront' ), WC()->cart->get_cart_contents_count() ) ); ?>
+				</span>
+			</a>
+		<?php
+	}
+}
 ?>
